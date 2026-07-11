ee0b549a146d5d850e909ffc8a796647254cfd73
notfl <2490782554@qq.com>
2026-07-11 12:01:37 +0800

    init

---
+++ docs/gen.py
@@ -0,0 +1,8 @@
+import os, json
+path = r'D:\Tech\Code\YYHshiajin\xuexi\Resume\docs\index.html'
+
+# Read the original to extract content sections
+# Build the new file
+with open(path, 'w', encoding='utf-8') as f:
+    f.write('TEST')
+print('written')
